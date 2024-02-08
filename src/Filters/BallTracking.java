ee7d4c1e8e8bc3974727b05f90624eb3c0d18804
abalakrishnan139 <aprakash597@student.fuhsd.org>
2024-02-08 09:31:29 -0800

    xx

--- src/Filters/BallTracking.java
+++ src/Filters/BallTracking.java
@@ -1,17 +1,29 @@
 package Filters;
 
+import Interfaces.Interactive;
 import Interfaces.PixelFilter;
 import core.DImage;
 
-public class BallTracking implements PixelFilter {
+public class BallTracking implements PixelFilter, Interactive {
     private PixelFilter identity = new Identity();
     private PixelFilter blur = new Blur();
-    private PixelFilter colorMaskRGB = new ColorMaskRGB();
+    private ColorMaskRGB colorMaskRGB = new ColorMaskRGB();
     private PixelFilter colorMaskHSV = new ColorMaskHSV();
     @Override
     public DImage processImage(DImage img) {
         img = blur.processImage(img);
         img = colorMaskRGB.processImage(img);
+
         return img;
+    }
+
+    @Override
+    public void mouseClicked(int mouseX, int mouseY, DImage img) {
+        colorMaskRGB.mouseClicked(mouseX, mouseY, img);
+    }
+
+    @Override
+    public void keyPressed(char key) {
+        colorMaskRGB.keyPressed(key);
     }
 }
